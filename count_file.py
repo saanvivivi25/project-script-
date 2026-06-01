1ca9a80d3d11917cd3e4bdcde46591cdd071025e
saanvivivi25 <saanviblehh@gmail.com>
2026-06-01 17:08:53 +0530

    Add files via upload

---
+++ count_file.py
@@ -0,0 +1,27 @@
+import os
+
+print("Current Folder:", os.getcwd())
+
+
+
+file = open(r"C:\Users\HP\Desktop\python-automation-internship\week 1\file.txt", "r")
+
+# Read the entire content of the file
+content = file.read()
+
+# Count characters
+char_count = len(content)
+
+# Count words
+word_count = len(content.split())
+
+# Count lines
+line_count = len(content.splitlines())
+
+# Close the file
+file.close()
+
+# Display the results
+print("Number of Lines:", line_count)
+print("Number of Words:", word_count)
+print("Number of Characters:", char_count)
